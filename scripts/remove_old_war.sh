acc38636aa0cc601542ffb7a38849babd38cae99
faisalkamilansari <faisalkamilansari@gmail.com>
2024-09-06 03:51:45 +0530

    rectified for path

--- scripts/remove_old_war.sh
+++ scripts/remove_old_war.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Remove the existing WAR file
 if [ -f /opt/tomcat/webapps/web-calculator.war ]; then
-  rm /opt/tomcat/webapps/web-calculator.war
+  rm /opt/tomcat/webapps/ROOT.war
 fi
 
 # Remove the exploded directory if it exists
 if [ -d /opt/tomcat/webapps/web-calculator ]; then
-  rm -rf /opt/tomcat/webapps/web-calculator
+  rm -rf /opt/tomcat/webapps/ROOT
 fi
